c704f57845bcd9cec775037a6957ae40cc252659
TahsinAhammed16 <tahsin.ahammed16@gmail.com>
2023-11-11 08:53:28 +0600

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <span className="loading loading-ring loading-lg"></span>
+    </>
+  );
+}
+
+export default App;
