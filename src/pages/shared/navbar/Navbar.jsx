0c70d1231f612cf1b938f170f0e5caf8c15e1276
TahsinAhammed16 <tahsin.ahammed16@gmail.com>
2023-11-17 11:28:49 +0600

    update user info on navbar

--- src/pages/shared/navbar/Navbar.jsx
+++ src/pages/shared/navbar/Navbar.jsx
@@ -108,25 +108,27 @@ const Navbar = () => {
               <div className="dropdown dropdown-end ml-2">
                 <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                   <div className="w-10 rounded-full">
-                    <img src="https://www.summithealth.org.au/wp-content/uploads/2021/07/placeholder.jpg" />
+                    <img src={user.photoURL} />
                   </div>
                 </label>
 
                 <ul
                   tabIndex={0}
-                  className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                  className="menu menu-sm dropdown-content mt-4 z-[1] p-2 shadow bg-base-200 rounded-box w-60"
                 >
                   <li>
-                    <a className="justify-between">
-                      Profile
-                      <span className="badge">New</span>
+                    <a className="justify-between">Name: {user.displayName}</a>
+                  </li>
+                  <li>
+                    <a>Email: {user.email}</a>
+                  </li>
+                  <li>
+                    <a
+                      onClick={handleSignOut}
+                      className="bg-red-600 text-white font-semibold"
+                    >
+                      Logout
                     </a>
-                  </li>
-                  <li>
-                    <a>Settings</a>
-                  </li>
-                  <li>
-                    <a onClick={handleSignOut} className="bg-red-600 text-white font-semibold">Logout</a>
                   </li>
                 </ul>
               </div>
